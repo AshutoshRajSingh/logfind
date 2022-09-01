a6e79b7c7bc513c103cb376a4acd8b72140094c8
AshutoshRajSingh <ashutosh200210@outlook.com>
2022-09-01 18:19:04 +0530

    implement basic main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+use logfind::run;
+use logfind::config::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let config = match Config::from_args(&args){
+        Ok(conf) => conf,
+        Err(e) => {
+            eprintln!("{:?}", e);
+            return;
+        }
+    };
+    
+    match run(config) {
+        Ok(_) => {},
+        Err(e) => {
+            eprintln!("Error occurred: {:?}", e);
+        }
+    }
 }
