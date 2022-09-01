c6fcb3b497f5bc13e1bae8bee9a5bf138df42aba
AshutoshRajSingh <ashutosh200210@outlook.com>
2022-09-01 18:08:35 +0530

    add tests for flatten_file_tree

--- src/util.rs
+++ src/util.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::collections::VecDeque;
 use std::path::PathBuf;
 
-fn flatten_file_tree(filepath: &str) -> Vec<PathBuf> {
+pub fn flatten_file_tree(filepath: &str) -> Vec<PathBuf> {
     let mut resvec: Vec<PathBuf> = Vec::new();
     let mut queue: VecDeque<PathBuf> = VecDeque::new();
 
@@ -30,9 +30,39 @@ fn flatten_file_tree(filepath: &str) -> Vec<PathBuf> {
 }
 
 #[test]
-fn initial_test() {
-    let k = flatten_file_tree("/home/wasabi/rust/playground");
-    for entry in k {
-        println!("{:?}", entry);
+fn test_directory_flattening() {
+    use fs::File;
+    const BASE_DIR: &str = "./target/debug/test_dir/";
+    let test_dirs = vec![
+        "./target/debug/test_dir",
+        "./target/debug/test_dir/branch1",
+        "./target/debug/test_dir/branch2",
+        "./target/debug/test_dir/branch3",
+        "./target/debug/test_dir/branch1/subbranch1",
+        "./target/debug/test_dir/branch1/subbranch2",
+        "./target/debug/test_dir/branch2/subbranch1"
+    ];
+    let mut test_files = vec![
+        "./target/debug/test_dir/root1.log",
+        "./target/debug/test_dir/root2.log",
+        "./target/debug/test_dir/branch1/lvl1.log",
+        "./target/debug/test_dir/branch1/subbranch1/lvl2.log",
+        "./target/debug/test_dir/branch2/subbranch1/lvl2.log",
+        "./target/debug/test_dir/branch2/lvl1.log",
+        "./target/debug/test_dir/branch3/lvl1.log"
+    ];
+
+    for dir in test_dirs {
+        if let Ok(_) = fs::create_dir_all(dir) {};
     }
+
+    for file in test_files.iter() {
+        if let Ok(_) = File::create(file) {};
+    }
+
+    let mut flattened = flatten_file_tree(BASE_DIR);
+
+    assert_eq!(test_files.sort(), flattened.sort());
+
+    if let Ok(_) = fs::remove_dir_all(BASE_DIR) {};
 }
