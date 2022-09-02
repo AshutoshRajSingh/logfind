231332c36b7aea011d45c5dce11679735ab63a1f
AshutoshRajSingh <ashutosh200210@outlook.com>
2022-09-02 23:58:14 +0530

    major overhaul to config

--- src/config.rs
+++ src/config.rs
@@ -1,44 +1,95 @@
-pub struct Config {
-    pub query: String
+use crate::searching::SearchType;
+use crate::constants::FLAG_PREFIX;
+
+pub enum OutputType {
+    Default,
+    Minimal,
+    Verbose
 }
 
-impl Config {
-    pub fn new(query: &String) -> Self {
-        Self { query: query.clone() }
+pub struct Config <'a> {
+    pub queries: Vec<&'a str>,
+    pub output_type: OutputType,
+    pub search_type: SearchType
+}
+
+fn parse_flags<'a>(args: &'a Vec<String>) -> (Vec<&'a String>, Vec<&'a String>) {
+    let flags = args.iter().filter(|arg| { arg.starts_with(FLAG_PREFIX) }).collect();
+    let queries = args.iter().filter(|arg| { !arg.starts_with(FLAG_PREFIX) }).collect();
+
+    (flags, queries)
+}
+
+impl<'a> Config<'a> {
+    pub fn new(queries: Vec<&'a str>, output_type: OutputType, search_type: SearchType) -> Self {
+        Self { 
+                queries: queries,
+                output_type: output_type,
+                search_type: search_type
+            }
     }
-    pub fn from_args(args: &[String]) -> Result<Self, &str> {
+    pub fn from_defaults() -> Self {
+        Self {
+            queries: Vec::new(),
+            output_type: OutputType::Default,
+            search_type: SearchType::All,
+        }
+    }
+    pub fn from_args(args: &'a Vec<String>) -> Result<Self, String> {
         if args.len() < 2 {
-            return Err("Not enough arguments supplied");
+            return Err(String::from("Not enough arguments supplied"));
         } else {
-            Ok(Self::new(&args[1]))
+            let (flags, queries) = parse_flags(args);
+            let mut new_conf = Config::from_defaults();
+
+            for flag in flags.iter() {
+                match flag.as_str() {
+                    "--any" => {
+                        new_conf.search_type = SearchType::Any;
+                    }
+                    "--minimal" => {
+                        new_conf.output_type = OutputType::Minimal;
+                    }
+                    _ => {
+                        let msg = format!("Unknown flag: {}", flag);
+                        return Err(msg);
+                    }
+                }
+            }
+
+            for query in queries[1..].iter() {
+                new_conf.queries.push(query);
+            }
+            Ok(new_conf)
         }
     }
 }
 
-#[test]
-fn test_config_creation() {
-    let test_string = String::from("This is sus");
-    let test_conf = Config::new(&test_string);
-    assert_eq!(test_conf.query, test_string);
-}
+#[cfg(test)]
+mod tests{
+    use super::*;
 
-#[test]
-fn test_config_creation_from_valid_args() {
-    let valid_args = vec![
-        String::from("logfind"),
-        String::from("nginx")
-    ];
-    let test_conf = Config::from_args(&valid_args).unwrap();
-    assert_eq!(test_conf.query, valid_args[1]);
-}
+    #[test]
+    fn test_config_creation_from_args() {
+        let sample_command_line_arguments = vec![
+            String::from("logfind"),
+            String::from("nginx"),
+            String::from("amd64"),
+            String::from("--any"),
+            String::from("--minimal")
+        ];
 
-#[test]
-fn test_config_creation_from_invalid_args() {
-    let invalid_args = vec![
-        String::from("logfind"),
-    ];
-    let other_test_conf = Config::from_args(&invalid_args);
-    if let Ok(_) = other_test_conf {
-        panic!("config got constructed when it shouldn't have");
+        let sample_conf = Config::from_args(&sample_command_line_arguments).unwrap();
+
+        assert!(sample_conf.queries.contains(&"nginx"));
+        assert!(!sample_conf.queries.contains(&"logfind"));
+
+        if let SearchType::All = sample_conf.search_type {
+            panic!("Search type was all when it should have been any");
+        }
+
+        if let OutputType::Default = sample_conf.output_type {
+            panic!("Output type was default when it shoudl have been minimal");
+        }
     }
 }
