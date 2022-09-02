80613590b1107c705837a58d093665a98e49a97c
AshutoshRajSingh <ashutosh200210@outlook.com>
2022-09-03 00:49:00 +0530

    add support for formatting options

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod config;
 pub mod searching;
 pub mod constants;
 pub mod util;
+pub mod formatting;
 
 use config::Config;
 use searching::search_multiple;
@@ -10,6 +11,9 @@ use util::flatten_file_tree;
 
 use std::fs;
 use std::error::Error;
+
+use crate::config::OutputType;
+use crate::searching::MultipleSearchResult;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     for path in DEFAULT_PATHS {
@@ -23,9 +27,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
             let results = search_multiple(config.queries.as_slice(), contents.as_str(), &config.search_type);
             if results.len() > 0 {
                 println!("File: {:?}", &entry);
+
+                let format_fn: fn(&MultipleSearchResult) -> String = match config.output_type {
+                    OutputType::Default => formatting::format_default,
+                    OutputType::Minimal => formatting::format_minimal,
+                    OutputType::Verbose => formatting::format_default,
+                };
+
                 for result in results {
-                    println!("\t{}: {}", result.line_no, result.line);
+                    println!("\t{}", format_fn(&result));
                 }
+                println!("");
             }
             
         }
